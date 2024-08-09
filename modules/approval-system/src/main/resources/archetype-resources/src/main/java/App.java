f5317f1ed2f9e709f0e1609115682f5e129a7ffc
DZakirin <dzakirinmd@gmail.com>
2024-08-09 08:56:47 +0800

    refactor project to modular

---
+++ modules/approval-system/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $net.dzakirin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
